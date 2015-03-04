1f46c6fc2567d3bbc474d9116c42a55ba2ba428d
atyafsaud <a.ksa11@hotmail.com>
2015-03-05 01:38:26 +0300

    whiteboard

---
+++ app.js
@@ -0,0 +1,48 @@
+/*var express = require("express"),
+    app = express.createServer(),
+    io = require('socket.io').listen(app);*/
+	
+	var express1 = require("express"),
+	     exp = express1(),
+		 http= require('http'); 
+    var app = http.createServer(exp),
+    io = require('socket.io').listen(app);
+//	io = require('socket.io')(app);
+	
+	
+
+var redis = require("redis"),
+    rclient = redis.createClient();
+
+app.listen(8080);
+//app.use("/static", express.static(__dirname + '/static'));
+
+exp.use("/static", express1.static(__dirname + '/static'));
+
+exp.get('/', function (req, res) {
+    res.sendfile(__dirname + '/index.html');
+});
+
+var board_sockets = {};
+
+io.sockets.on('connection', function (socket) {
+    var board_id = socket.handshake.query.board_id;
+    if(!board_sockets[board_id]) board_sockets[board_id] = [];
+    // TODO: Do something if the board_id is undefined
+    board_sockets[board_id].push(socket);
+    var redis_key = "board_" + board_id;
+    rclient.lrange(redis_key, 0, -1, function(err, res) {
+        socket.emit('board_state', res);
+    });
+    
+    socket.on('update', function (data) {
+        for(idx in board_sockets[board_id]) {
+            var s = board_sockets[board_id][idx];
+            if(s != socket) {
+                s.emit('update', data);
+                data.timestamp = Math.round(new Date().getTime() / 1000);
+                rclient.lpush(redis_key, JSON.stringify(data));
+            }
+        }
+    });
+});
---
+++ static/whiteboard.js
@@ -0,0 +1,85 @@
+// TODO: Make it work on the ipad/iphone
+
+function draw_segment(canvas, socket, x1, y1, x2, y2) {
+    if(x1 && y1) {
+        canvas.drawLine({
+            strokeWidth: 2,
+            strokeStyle: "#000",
+            x1: x1, y1: y1,
+            x2: x2, y2: y2
+        });
+        if(socket) socket.emit('update', {action:"draw_segment", x1:x1, y1:y1, x2:x2, y2:y2});
+    }
+}
+
+var last_point = null;
+var has_touch = false;
+
+function drag(ev) {
+    if(last_point) {
+        draw_segment(ev.data.canvas, ev.data.socket, last_point.x, last_point.y, ev.offsetX, ev.offsetY);
+    }
+    last_point = {x: ev.offsetX, y:ev.offsetY};
+}
+
+function touchdrag(ev) {
+    ev.preventDefault();
+    var touchX = ev.changedTouches[0].clientX - ev.data.canvas.get(0).offsetLeft;
+    var touchY = ev.changedTouches[0].clientY - ev.data.canvas.get(0).offsetTop;
+    if(last_point) {
+        draw_segment(ev.data.canvas, ev.data.socket, last_point.x, last_point.y, touchX, touchY);
+    }
+    last_point = {x: touchX, y:touchY};
+}
+
+function handle_update(canvas, data) {
+    if(data.action == "draw_segment") {
+        draw_segment(canvas, null, data.x1, data.y1, data.x2, data.y2);
+    }
+}
+
+function initalize_whiteboard(elements, options) {
+    if(!options) options = {};
+    if(!options.url) options.url = 'http://127.0.0.1:8080';
+    var socket = io.connect(options.url);
+    elements.each(function(idx, el) {
+        el = $(el);        
+        socket.on('update', function(data) {
+            handle_update(el, data);
+        });
+        
+        socket.on('board_state', function(state) {
+            for(idx in state) {
+                handle_update(el, JSON.parse(state[idx]));
+            }
+        });
+        
+        el.on("touchstart", function(ev) {
+            has_touch = true;
+            ev.preventDefault();
+            el.get(0).ontouchmove = function(ev) {
+                ev.data = {canvas:el, socket:socket};
+                touchdrag(ev);
+            };
+        });
+        el.on("touchend", function(ev) {
+            el.get(0).ontouchmove = null;
+            last_point = null;
+        });
+        
+        el.on("mousedown", function(ev) {
+            if(has_touch) return;
+            el.on("mousemove", {canvas:el, socket:socket}, drag);
+        });
+        el.on("mouseup", function(ev) {
+            el.off("mousemove", drag);
+            last_point = null;
+        });
+    });
+}
+
+(function ($) {
+    $.fn.whiteboard = function(options) {
+        initalize_whiteboard($(this), options);
+    }
+})(jQuery);
